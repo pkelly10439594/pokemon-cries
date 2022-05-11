23019f994425a64037d08a1a482ca1ff7e862d74
Paul Kelly III <pkelly1@stevens.edu>
2022-05-11 03:30:14 -0400

    fixed gulpin, updated visuals slightly, turned all gen filters into toggles

--- public/js/list.js
+++ public/js/list.js
@@ -1,16 +1,16 @@
 (function($) {
-    var genList = $("#genList");
-    var genIndicator = $("#genIndicator");
-    var generation = -1;
     var genIndex = -1;
+    var genIndicator = $("#genIndicator");
+    var genList = $("#genList");
+    var pkmnIndex = 0;
     var pkmnList = $("#pkmnList");
 
     function getPkmnCryHTML(pkmnNames, extensions, fileName, cryTag, gen) {
         return $(`
-            <div class="cryButton gen${gen}" id="${cryTag}">
+            <div class="cryButton gen${gen + 1}" id="${cryTag}">
                 ${pkmnNames.reduce((a, b, i) => a + `
                     <img src="/public/images/${fileName}${extensions[i].replace("%", "%25")}.png" class="cryImg">
-                    ${b.replace(DELIMITER, "-")}
+                    ${b.indexOf(DELIMITER) === -1 ? b : b.replace(DELIMITER, " (").concat(")")}
                 `, "")}
                 <audio>
                     <source src="/public/cries/${cryTag.replace("%", "%25")}.mp3" type="audio/mpeg">
@@ -19,60 +19,52 @@
         `);
     }
 
-//  STILL USEFUL FOR GENERATING THE PAGES, MIGHT REVERT TO THIS LATER
+    for (let [generation, pkmnSubList] of POKEMON.entries()) {
+        for (let species of pkmnSubList) {
+            ++pkmnIndex;
+            let indAsStr = ("000" + pkmnIndex).slice(-4);
+
+            if (typeof(species) === 'string') // element only has one cry
+                pkmnList.append(getPkmnCryHTML([species], [""], indAsStr, indAsStr, generation));
+            else // element is a species with multiple cries
+                species.forEach((cry) => {
+                    if (typeof(cry) === 'string') { // this cry only has one form
+                        let ending = cry.includes(DELIMITER)
+                            ? cry.slice(cry.indexOf(DELIMITER)).toLowerCase()
+                            : "";
+                        pkmnList.append(getPkmnCryHTML([cry], [ending], indAsStr, indAsStr + ending, generation));
+                    } else { // this cry has multiple forms
+                        if (typeof(species[0]) === 'string') // this pokemon has a definite main form
+                            pkmnList.append(getPkmnCryHTML(cry,
+                                                cry.map((form) => {
+                                                    return form.includes(DELIMITER)
+                                                        ? form.slice(form.indexOf(DELIMITER)).toLowerCase()
+                                                        : "";}),
+                                                indAsStr, indAsStr + cry[0].slice(cry[0].indexOf(DELIMITER)).toLowerCase(),
+                                                generation));
+                        else // this pokemon has multiple initial forms
+                            pkmnList.append(getPkmnCryHTML(cry,
+                                                cry.map((form) => {
+                                                    return form.includes(DELIMITER)
+                                                        ? form.slice(form.indexOf(DELIMITER)).toLowerCase()
+                                                        : "";}),
+                                                indAsStr, indAsStr, generation));
+                    }
+                });
+        }
+    }
+
+    pkmnList.children().each(function (index, element) {
+        $(element).on("click", function(event) {
+            event.preventDefault();
+            $(this).children("audio")[0].play();
+        });
+    });
+
     genList.children().each(function (index, element) {
         $(element).on("click", function (event) {
             event.preventDefault();
-            if (index + 1 === generation) return;
-            generation = index + 1;
-            switch (generation) {
-                case 1: genIndex = 1;break;
-                case 2: genIndex = 152;break;
-                case 3: genIndex = 252;break;
-                case 4: genIndex = 387;break;
-                case 5: genIndex = 494;break;
-                case 6: genIndex = 650;break;
-                case 7: genIndex = 722;break;
-                case 8: genIndex = 810;break;
-                default: genIndex = -1;
-            }
-            genIndicator.text(`Generation ${generation}`);
-            let pkmnSubList = POKEMON[index];
-
-            // Display all the pokemon to the screen
-            pkmnList.empty();
-            pkmnSubList.forEach((species, i) => {
-                indAsStr = ("000" + (i+genIndex)).slice(-4);
-                if (typeof(species) === 'string') // element only has one cry
-                    pkmnList.append(getPkmnCryHTML([species], [""], indAsStr, indAsStr, generation));
-                else // element is a species with multiple cries
-                    species.forEach((cry) => {
-                        if (typeof(cry) === 'string') { // this cry only has one form
-                            let ending = cry.includes(DELIMITER)
-                                ? cry.slice(cry.indexOf(DELIMITER)).toLowerCase()
-                                : "";
-                            pkmnList.append(getPkmnCryHTML([cry], [ending], indAsStr, indAsStr + ending, generation));
-                        } else { // this cry has multiple forms
-                            if (typeof(species[0]) === 'string') // this pokemon has a definite main form
-                                pkmnList.append(getPkmnCryHTML(cry,
-                                                    cry.map((form) => {
-                                                        return form.includes(DELIMITER)
-                                                            ? form.slice(form.indexOf(DELIMITER)).toLowerCase()
-                                                            : "";}),
-                                                    indAsStr, indAsStr + cry[0].slice(cry[0].indexOf(DELIMITER)).toLowerCase(),
-                                                    generation));
-                            else // this pokemon has multiple initial forms
-                                pkmnList.append(getPkmnCryHTML(cry,
-                                                    cry.map((form) => {
-                                                        return form.includes(DELIMITER)
-                                                            ? form.slice(form.indexOf(DELIMITER)).toLowerCase()
-                                                            : "";}),
-                                                    indAsStr, indAsStr, generation));
-                        }
-                    });
-            });
-            for (let box of pkmnList.children())
-                box.addEventListener("click", function() {$(this).children("audio")[0].play()});
+            $(`.gen${index + 1}`).each(function (i, e) {$(e).toggle();});
         });
     });
 })(window.jQuery);
